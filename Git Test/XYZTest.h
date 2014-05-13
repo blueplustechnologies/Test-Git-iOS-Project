c129e3d77f05ac80421eacd6aa0215bbaa52e352
mac user <macuser@Macintosh.local>
2014-05-13 23:31:27 +0200

    added a test class

---
+++ Git Test/XYZTest.h
@@ -0,0 +1,13 @@
+//
+//  XYZTest.h
+//  Git Test
+//
+//  Created by mac user on 13/05/2014.
+//  Copyright (c) 2014 Blue Plus Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZTest : NSObject
+
+@end
---
+++ Git Test/XYZTest.m
@@ -0,0 +1,13 @@
+//
+//  XYZTest.m
+//  Git Test
+//
+//  Created by mac user on 13/05/2014.
+//  Copyright (c) 2014 Blue Plus Technologies. All rights reserved.
+//
+
+#import "XYZTest.h"
+
+@implementation XYZTest
+
+@end
